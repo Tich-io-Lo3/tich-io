3de6d789a934f2f3263fc8f14dd006bfc386a4ae
Gautier E <gautier.edel@etu.unistra.fr>
2022-01-07 15:36:49 +0100

    Add user detail page + fix language typo

--- front/src/App.js
+++ front/src/App.js
@@ -7,8 +7,11 @@ function App() {
   return (
     <CurrentUserProvider>
       <Link to="/create-game">Create game</Link>
+      <br />
       <Link to="/connexion">Sign in</Link>
+      <br />
       <Link to="/creation">Sign up</Link>
+      <br />
       <Link to="/games">Games</Link>
     </CurrentUserProvider>
   );
