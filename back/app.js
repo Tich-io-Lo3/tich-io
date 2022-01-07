48310ae30b8ffb9b1dca9a0c080603647a82d9b3
Hugo KELHETTER <hkelhetter@unistra.fr>
2022-01-07 17:04:44 +0100

    fix fichier mal placé  + changement port

--- back/app.js
+++ back/app.js
@@ -60,7 +60,7 @@ require("./route")(app);
 }); */
 
 // launch server
-const server = app.listen(3000, () => {
+const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log("App listening at http://%s:%s", host, port);
