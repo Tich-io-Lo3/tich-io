f693b2b2edf696e37a75d7f148e20331dfbe94e7
Hugo KELHETTER <hkelhetter@unistra.fr>
2022-01-22 16:03:08 +0100

    suppression des lignes autogénérées dans app.js, fixe de typo et je sais pas pourquoi j'avais des dossiers en double

--- back/app.js
+++ back/app.js
@@ -1,67 +1,28 @@
-/* var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+require("dotenv").config();
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const express = require("express");
+const cors = require("cors");
 
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-module.exports = app;
- */
-
-const express = require("express"),
-  bodyParser = require("body-parser");
-
+const bdd = require("./models");
 const app = express();
 
-app.use(bodyParser.json());
+const upload = require("multer")();
+const AWS = require("aws-sdk");
+const nanoid = require("nanoid");
 
-// register routes
-require("./route")(app);
+const questionType = ["question", "action"];
 
-// register error handling middleware
-/* app.use((err, req, res) => {
-  if (err.status === undefined) {
-    return res.status(500).send(err.message);
-  } else {
-    return res.status(err.status).send(err.message);
-  }
-}); */
+const s3 = new AWS.S3({
+  accessKeyId: process.env.S3_ID,
+  secretAccessKey: process.env.S3_TOKEN,
+  endpoint: process.env.S3_DOMAIN,
+  sslEnabled: false,
+  s3ForcePathStyle: true,
+});
 
-// launch server
-const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log("App listening at http://%s:%s", host, port);
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT || 3630, () => {
+  console.log(`Serveur lancé sur le port ${process.env.PORT || 3630}`);
 });
