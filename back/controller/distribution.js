a70e7327a0e54cd40cbb22934ce1136ca5116eed
Hugo KELHETTER <hkelhetter@unistra.fr>
2022-01-29 18:54:49 +0100

    fix return distribution

--- back/controller/distribution.js
+++ back/controller/distribution.js
@@ -8,14 +8,12 @@ module.exports = {
       where: {
         GameId: req.params.game_id,
       },
-    })
-      .then((distrib) => {
-        if (!distrib) {
-          throw { status: 404, message: "Requested distrib not found" };
-        }
-        return distrib.json();
-      })
-      .catch(next);
+    }).then((distrib) => {
+      if (!distrib) {
+        throw { status: 404, message: "Requested distrib not found" };
+      }
+      return distrib.json();
+    });
   },
   get_by_os: (req, res) => {
     const distrib = db.Distribution.findOne({
