297ebb40cab1d76249578a2746aaa0baac69023a
Hugo KELHETTER <hkelhetter@unistra.fr>
2022-01-30 22:54:13 +0100

    fix typo

--- back/controller/library.js
+++ back/controller/library.js
@@ -3,7 +3,7 @@ const { Op } = require("sequelize");
 module.exports = {
   get_all_by_user_id: (req, res, next) => {
     return db.Library.findAll({
-      where: { userId: { [Op.eq]: req..user_id } },
+      where: { userId: { [Op.eq]: req.body.user_id } },
     })
       .then((games) => res.json(games))
       .catch(next);
